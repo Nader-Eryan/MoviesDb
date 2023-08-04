394f6d4eb54274f397602d1898d57c78b10a7057
Nader-Eryan <ne1309@fayoum.edu.eg>
2023-08-04 21:00:06 +0300

    added title to bottom list view and fixed border radius of it

--- lib/features/home/presentation/views/widgets/bottom_list_view.dart
+++ lib/features/home/presentation/views/widgets/bottom_list_view.dart
@@ -13,8 +13,7 @@ class BottomListView extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        SizedBox(
-          height: 235.h,
+        Expanded(
           child: ListView.builder(
             controller: bottomScrollController,
             physics: const BouncingScrollPhysics(),
@@ -22,24 +21,40 @@ class BottomListView extends StatelessWidget {
             itemBuilder: ((context, index) {
               return Padding(
                 padding: const EdgeInsets.all(10),
-                child: GenreItem(
-                  width: 140.w,
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: GenreItem(
+                        width: 140.w,
+                        borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(16),
+                            topRight: Radius.circular(16),
+                            bottomLeft: Radius.zero),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 4,
+                    ),
+                    const SizedBox(
+                      width: 140,
+                      height: 20,
+                      child: Text(
+                        'Movie title Movie app',
+                        style: Styles.textStyleBold16,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ],
                 ),
               );
             }),
           ),
         ),
-        const SizedBox(
-          width: 130,
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 18),
-            child: Text(
-              'Movie title Movie title movie',
-              style: Styles.textStyleBold16,
-              overflow: TextOverflow.ellipsis,
-            ),
-          ),
-        ),
+        // const SizedBox(
+        //   width: 140,
+        //   child:
+        //     child:
+        // ),
       ],
     );
   }
