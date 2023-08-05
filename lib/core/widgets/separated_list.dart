4892037413be51a2fda506e5e8484136704a153e
Nader-Eryan <ne1309@fayoum.edu.eg>
2023-08-05 20:31:07 +0300

    fixed some styles

--- lib/core/widgets/separated_list.dart
+++ lib/core/widgets/separated_list.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:size_config/size_config.dart';
 
-import '../../features/home/presentation/views/widgets/genre_item.dart';
+import 'item_details.dart';
+import 'list_item.dart';
 import '../utils/styles.dart';
 
 class SeparatedList extends StatelessWidget {
@@ -12,48 +13,54 @@ class SeparatedList extends StatelessWidget {
     return ListView.separated(
       physics: const BouncingScrollPhysics(),
       itemBuilder: (context, index) {
-        return Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 6),
-          child: Row(children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-              child: GenreItem(
-                width: 100,
-                borderRadius: BorderRadius.circular(8),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(12),
-              child: SizedBox(
-                width: 200.w,
-                height: 110.h,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    const Text(
-                      'Don\'t Look Up',
-                      style: Styles.textStyleBold20,
-                      overflow: TextOverflow.ellipsis,
-                      maxLines: 2,
-                    ),
-                    Text(
-                      '2021',
-                      style:
-                          Styles.textStyleBold16.copyWith(color: Colors.grey),
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    Text(
-                      'Leonardo DiCaprio, Jennifer Lawerence',
-                      style:
-                          Styles.textStyleMedium16.copyWith(color: Colors.grey),
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ],
+        return InkWell(
+          onTap: () {
+            Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => const ItemDetails()));
+          },
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 6),
+            child: Row(children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                child: ListItem(
+                  width: 100,
+                  borderRadius: BorderRadius.circular(8),
                 ),
               ),
-            )
-          ]),
+              Padding(
+                padding: const EdgeInsets.all(12),
+                child: SizedBox(
+                  width: 200.w,
+                  height: 110.h,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      const Text(
+                        'Don\'t Look Up',
+                        style: Styles.textStyleBold20,
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 2,
+                      ),
+                      Text(
+                        '2021',
+                        style:
+                            Styles.textStyleBold16.copyWith(color: Colors.grey),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      Text(
+                        'Leonardo DiCaprio, Jennifer Lawerence',
+                        style: Styles.textStyleMedium16
+                            .copyWith(color: Colors.grey),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ]),
+          ),
         );
       },
       itemCount: 10,
