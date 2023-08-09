f5272f93e9d0875a79763e7446040c96906e958d
Nader-Eryan <ne1309@fayoum.edu.eg>
2023-08-09 07:37:17 +0300

    profile view part 1

--- lib/features/profile/presentation/views/profile_view.dart
+++ lib/features/profile/presentation/views/profile_view.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:whats_for_tonight/core/utils/constants.dart';
+import 'package:whats_for_tonight/core/utils/styles.dart';
 import 'package:whats_for_tonight/core/widgets/profile_pic.dart';
 import 'package:whats_for_tonight/features/home/presentation/manager/cubit/brightness_cubit.dart';
+import 'package:whats_for_tonight/features/profile/presentation/views/about_view.dart';
 
 class ProfileView extends StatelessWidget {
   const ProfileView({super.key});
-
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: SingleChildScrollView(
-          child: Padding(
+      child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
@@ -22,38 +23,82 @@ class ProfileView extends StatelessWidget {
                 ),
               ],
             ),
-            Center(
-              child: BlocBuilder<BrightnessCubit, BrightnessState>(
-                builder: (context, state) {
-                  return Switch(
-                      value: BlocProvider.of<BrightnessCubit>(context).isDark,
-                      onChanged: (_) {
-                        BlocProvider.of<BrightnessCubit>(context)
-                            .switchBrightnessMode();
-                      });
-                },
+            Expanded(
+              child: ListView(
+                physics: const NeverScrollableScrollPhysics(),
+                children: [
+                  InkWell(
+                    onTap: () {},
+                    child: const ListTile(
+                      title: Text(
+                        'Account',
+                        style: Styles.textStyleBold18,
+                      ),
+                    ),
+                  ),
+                  const Divider(
+                    thickness: 2,
+                  ),
+                  ListTile(
+                    title: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          'App Theme',
+                          style: Styles.textStyleBold18,
+                        ),
+                        BlocBuilder<BrightnessCubit, BrightnessState>(
+                          builder: (context, state) {
+                            return DropdownButton(
+                                iconEnabledColor: kActiveIcon,
+                                items: const [
+                                  DropdownMenuItem(
+                                    value: 'light',
+                                    child: Text(
+                                      'Light',
+                                      style: Styles.textStyleBold16,
+                                    ),
+                                  ),
+                                  DropdownMenuItem(
+                                    value: 'dark',
+                                    child: Text(
+                                      'Dark',
+                                      style: Styles.textStyleBold16,
+                                    ),
+                                  ),
+                                ],
+                                onChanged: (val) {
+                                  BlocProvider.of<BrightnessCubit>(context)
+                                      .switchBrightnessMode(val);
+                                });
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Divider(
+                    thickness: 2,
+                  ),
+                  InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const AboutView()));
+                    },
+                    child: const ListTile(
+                      title: Text(
+                        'About',
+                        style: Styles.textStyleBold18,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            )
+            ),
           ],
         ),
-      )),
+      ),
     );
   }
 }
-
-
-/*
-Center(
-      child: BlocBuilder<BrightnessCubit, BrightnessState>(
-        builder: (context, state) {
-          return Switch(
-              value: BlocProvider.of<BrightnessCubit>(context).isDark,
-              onChanged: (_) {
-                BlocProvider.of<BrightnessCubit>(context)
-                    .switchBrightnessMode();
-              });
-        },
-      ),
-    )
-
- */
