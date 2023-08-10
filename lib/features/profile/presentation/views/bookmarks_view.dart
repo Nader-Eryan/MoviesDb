25be50c067cc15407354cbfe7f9e09dddb12ddf7
Nader-Eryan <ne1309@fayoum.edu.eg>
2023-08-11 02:59:44 +0300

    added custom arrow back app bar

--- lib/features/profile/presentation/views/bookmarks_view.dart
+++ lib/features/profile/presentation/views/bookmarks_view.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:whats_for_tonight/core/widgets/separated_list.dart';
 
+import '../../../../core/utils/functions/custom_arrow_back_app_bar.dart';
+
 class BookmarksView extends StatelessWidget {
   const BookmarksView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: Scaffold(
-        body: SeparatedList(),
+        appBar: customArrowBackAppBar(context),
+        body: const SeparatedList(),
       ),
     );
   }
