4c13c3d46c7e6eba975762c04ad211bdab3dd4bf
Nader-Eryan <ne1309@fayoum.edu.eg>
2023-08-11 02:59:52 +0300

    added splash view

---
+++ lib/features/home/presentation/views/splash_view.dart
@@ -0,0 +1,67 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:size_config/size_config.dart';
+import 'package:whats_for_tonight/features/home/presentation/manager/cubit/brightness_cubit.dart';
+import 'package:whats_for_tonight/features/home/presentation/views/home_view.dart';
+
+import '../../../custom_button_navigation.dart/presentation/manager/cubit/page_index_cubit.dart';
+
+class SplashView extends StatefulWidget {
+  const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  double padding = 100;
+  late Timer timer;
+  @override
+  void initState() {
+    BlocProvider.of<BrightnessCubit>(context).sharedPrefsInit();
+
+    padding = 0;
+    timer = Timer.periodic(const Duration(milliseconds: 400), (timer) {
+      padding = padding == 0 ? 100 : 0;
+      setState(() {});
+    });
+    Future.delayed(const Duration(milliseconds: 1500), () {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(
+          builder: (context) => BlocProvider(
+              create: (context) => PageIndexCubit(), child: const HomeView())));
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    timer.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //bool isDark = BlocProvider.of<BrightnessCubit>(context).isDark;
+    return SafeArea(
+      child: Scaffold(
+          //backgroundColor: isDark ? kPrimaryDarkTheme : kPrimaryLightTheme,
+          body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 260.h,
+            ),
+            AnimatedPadding(
+              curve: Curves.easeInOut,
+              duration: const Duration(milliseconds: 400),
+              padding: EdgeInsets.all(padding),
+              child: Image.asset('assets/images/moviesdb.png'),
+            ),
+          ],
+        ),
+      )),
+    );
+  }
+}
