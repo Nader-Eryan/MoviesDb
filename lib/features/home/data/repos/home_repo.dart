f5c70647ba5cceb1f964a9c134481f97634b8d95
Nader-Eryan <ne1309@fayoum.edu.eg>
2023-08-14 15:54:36 +0300

    fixed a null value

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -4,7 +4,6 @@ import 'package:whats_for_tonight/features/home/data/models/show/show.dart';
 
 abstract class HomeRepo {
   Future<Either<Failure, List<Show>>> fetchGenreShows(String genre);
-  Future<Either<Failure, List<Show>>> fetchSuggestionShows(
-      {required String list});
+  Future<Either<Failure, List<Show>>> fetchSuggestionShows({String? list});
   Future<Either<Failure, List<String>>> fetchgenreNames();
 }
